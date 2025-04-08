dde0853090de25018b31dfe31bf72714e73e8a04
Jan Kowalczyk <jan.kowalczyk@protonmail.com>
2025-04-08 07:53:39 +0200

    Added alocation, the size of C0 was wrong

--- linearizations/linear_get_centroid.m
+++ linearizations/linear_get_centroid.m
@@ -1,10 +1,11 @@
 function C0 = linear_get_centroid(cont_face, ele_id)
 % returns 3x3*nN matrix C0, delt_x0 = C0*delt_d
 nN_ele = cont_face.info.nN_ele;
+nN = cont_face.info.nN;
 fe = cont_face.fe;
+C0 = zeros(3,nN*3);
 shapef =  fe.N(fe.Cxi); % Shape funcs at center of isoparametric (reference) element
 glob_id = cont_face.nod(ele_id,:)*3-2; % vector of nN_ele node indexes of element
-
 for k=1:nN_ele
   C0(:,glob_id(k):glob_id(k)+2) = eye(3)*shapef(k);
 end
