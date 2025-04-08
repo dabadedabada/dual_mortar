e428a905c25b0610ea6e1331d88f54ad65a49937
Jan Kowalczyk <jan.kowalczyk@protonmail.com>
2025-04-08 07:55:15 +0200

    Reworked projection linearization to return matrix

--- linearizations/linear_project_onto_plane.m
+++ linearizations/linear_project_onto_plane.m
@@ -1,8 +1,17 @@
-function delt_proj_point = linear_project_onto_plane(point, x0, n0, delt_point, delt_x0, delt_n0)
-    % Popp 3d (A17)
-    % point and delt_point (nodal coordinate linearizations) are 1x3
-    % vectors
-    
-    delt_proj_point = delt_point - (dot(delt_point-delt_x0,n0) + ...
-      dot(point-x0, delt_n0))*n0 - dot(point-x0,n0)*delt_n0;
+function Ttilde = linear_project_onto_plane(coo, nod, x0, n0, N0, C0, sl_or_m, nN_m)
+% Popp 3d (A17)
+% nod is as integer - index of the row in the cont_face.coo
+% coo is a vector 1x3 of coordinates of point
+
+S = zeros(size(N0));
+if (sl_or_m == 1)
+  index = nN_m*3 + nod*3-2;
+  S(:,index:index+2) = eye(3);
 end
+if (sl_or_m == 2)
+  index = nod*3-2;
+  S(:,index:index+2) = eye(3);
+end
+
+Ttilde = S - (n0'*n0)*(S-C0) + ((n0*(coo-x0)')*eye(3)-(coo-x0)'*n0)*N0;
+
