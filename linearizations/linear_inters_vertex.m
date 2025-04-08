fc59557fad2700332a9ac35e25eec17d37fa38ff
Jan Kowalczyk <jan.kowalczyk@protonmail.com>
2025-04-08 07:56:18 +0200

    Reworked the clipping algorithm linearization

--- linearizations/linear_inters_vertex.m
+++ linearizations/linear_inters_vertex.m
@@ -1,17 +1,19 @@
-function delt_inter_vert = linear_inters_vertex(s_s, s_e, m_s, m_e, delt_s_s, delt_s_e, delt_m_s, delt_m_e, n0, delt_n0)
+function T = linear_inters_vertex(s_s, s_e, m_s, m_e, T_s_s, T_s_e, T_m_s, T_m_e, n0, N0)
 % returns derivative of vertex originated as intersection of line sections
 % of slave and master start and end nodes, Popp 3d (A19)
 
-term2 = cross(s_s-m_s, m_e-m_s);
-delt_term2 = cross(delt_s_s-delt_m_s, m_e-m_s) + cross(s_s-m_s, delt_m_e-delt_m_s);
-term3 = cross(s_e-s_s, m_e-m_s);
-delt_term3 = cross(delt_s_e-delt_s_s, m_e-m_s) + cross(s_e-s_s, delt_m_e-delt_m_s);
-numerator = dot(term2, n0);
-delt_numerator = dot(delt_term2, n0) + dot(term2, delt_n0);
-denominator = dot(term3, n0);
-delt_denominator = dot(delt_term3, n0) + dot(term3, delt_n0);
-frac = numerator/denominator;
-delt_frac = (delt_numerator*denominator - numerator*delt_denominator)/(denominator)^2;
-term4 = s_e-s_s;
-delt_term4 = delt_s_e-delt_s_s;
-delt_inter_vert = delt_s_s - (delt_frac*term4 + frac*delt_term4);
+sl_edge = s_e-s_s;
+m_edge = m_e-m_s;
+weird_term = s_s-m_s;
+
+f = dot(cross(weird_term, m_edge), n0);
+g = dot(cross(sl_edge, m_edge), n0);
+
+delt_sl_edge = T_s_e-T_s_s;
+delt_m_edge = T_m_e-T_m_s;
+delt_weird_term = T_s_s-T_m_s;
+
+delt_f = cross(weird_term, m_edge)*N0 + n0*skew_mat(m_edge)'*delt_weird_term + n0*skew_mat(weird_term)*delt_m_edge;
+delt_g = cross(sl_edge, m_edge)*N0 + n0*skew_mat(m_edge)'*delt_sl_edge + n0*skew_mat(sl_edge)*delt_m_edge;
+
+T = T_s_s - 1/(g*g)*sl_edge'*(delt_f*g - f*delt_g) - f/g*delt_sl_edge;
