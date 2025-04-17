e5b20904b73b7c29fee830553e912958311b3b4c
Jan Kowalczyk <jan.kowalczyk@protonmail.com>
2025-04-17 06:06:58 +0200

    naming changes and forgot identity matrix

--- linearizations/linear_averaged_normals.m
+++ linearizations/linear_averaged_normals.m
@@ -1,8 +1,8 @@
-function Pj = linear_averaged_normals(cont_face, normals_storage, nod_id)
+function Nj = linear_averaged_normals(cont_face, normals_storage, nod_id)
 % return directional derivative matrix of averaged nodal normal
 % delt_nj = Pj*delt_d_s
 % Popp 3D (A1)
 
 curr_normal = normals_storage.sum_normals(nod_id,:)';
-Pj = (1/norm(curr_normal)-curr_normal*curr_normal'/norm(curr_normal)^3)...
+Nj = (eye(3)/norm(curr_normal)-curr_normal*curr_normal'*norm(curr_normal)^-3)...
   *linear_get_sum_normals(cont_face, nod_id, normals_storage.ele_normals);
