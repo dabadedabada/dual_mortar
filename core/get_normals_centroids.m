c22473e807682d6976a9671adbe2e5d7b78e1269
Jan Kowalczyk <jan.kowalczyk@protonmail.com>
2025-04-17 06:08:19 +0200

    get_n0 and get_centroid right into storage

--- core/get_normals_centroids.m
+++ core/get_normals_centroids.m
@@ -8,17 +8,13 @@ averaged_normals = zeros(size(sum_normals));
 for i=1:nN
   averaged_normals(i,:) = sum_normals(i,:)/norm(sum_normals(i,:));
 end
-n0 = get_n0(cont_face, averaged_normals);
 [t1,t2] = get_tangents(cont_face, averaged_normals);
-
-% Centroids also calculated by mapping the center of reference element
-centroids = get_centroids(cont_face);
 
 normals_storage.ele_normals = ele_normals;
 normals_storage.sum_normals = sum_normals;
 normals_storage.averaged_normals = averaged_normals;
-normals_storage.n0 = n0;
-normals_storage.x0 = centroids;
+normals_storage.n0 = get_n0(cont_face, averaged_normals);
+normals_storage.x0 = get_centroids(cont_face);
 normals_storage.t1 = t1;
 normals_storage.t2 = t2;
 
