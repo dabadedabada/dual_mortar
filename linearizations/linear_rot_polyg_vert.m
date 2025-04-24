069bc21969eb25e1197be81368da6123a9c44a30
Jan Kowalczyk <jan.kowalczyk@protonmail.com>
2025-04-24 10:39:49 +0200

    the second argument in project should be global id, not local

--- linearizations/linear_rot_polyg_vert.m
+++ linearizations/linear_rot_polyg_vert.m
@@ -1,4 +1,4 @@
-function V = linear_rot_polyg_vert(s_coo, m_coo, x0, n0, N0, C0, ...
+function V = linear_rot_polyg_vert(s_coo, m_coo, s_nod, m_nod, x0, n0, N0, C0, ...
   clip_origin, proj_s_coo, proj_m_coo, rot_s_coo, rot_m_coo, nN_m, R, Ralg, rot_clip)
 
 n_vert = size(clip_origin, 1);
@@ -6,20 +6,20 @@ clip_Vtilde = cell(n_vert,1);
 for v=1:n_vert
   % case 1 - origin projected slave node
   if clip_origin(v, 1) == 1
-    P = linear_project_onto_plane(s_coo(:,clip_origin(v,2)), clip_origin(v,2), x0, n0, N0, C0, 1, nN_m);  
+    P = linear_project_onto_plane(s_coo(:,clip_origin(v,2)), s_nod(clip_origin(v,2)), x0, n0, N0, C0, 1, nN_m);  
     clip_Vtilde{v} = linear_rotate_points(R, Ralg, proj_s_coo(:,clip_origin(v,2)), P, x0, C0);
   % case 1 - origin projected master node
   elseif clip_origin(v, 1) == 2
-    P = linear_project_onto_plane(m_coo(:,clip_origin(v,2)), clip_origin(v,2), x0, n0, N0, C0, 2, nN_m);  
+    P = linear_project_onto_plane(m_coo(:,clip_origin(v,2)), m_nod(clip_origin(v,2)), x0, n0, N0, C0, 2, nN_m);  
     clip_Vtilde{v} = linear_rotate_points(R, Ralg, proj_m_coo(:,clip_origin(v,2)), P, x0, C0);
   % case 2 - line clipping
   elseif clip_origin(v, 1) == 3
     A = rot_s_coo(:,clip_origin(v,2)); B = rot_s_coo(:,clip_origin(v,3));   
     C = rot_m_coo(:,clip_origin(v,4)); D = rot_m_coo(:,clip_origin(v,5));
-    P_A = linear_project_onto_plane(s_coo(:,clip_origin(v,2)), clip_origin(v,2), x0, n0, N0, C0, 1, nN_m);
-    P_B = linear_project_onto_plane(s_coo(:,clip_origin(v,3)), clip_origin(v,3), x0, n0, N0, C0, 1, nN_m);
-    P_C = linear_project_onto_plane(s_coo(:,clip_origin(v,4)), clip_origin(v,4), x0, n0, N0, C0, 2, nN_m);
-    P_D = linear_project_onto_plane(s_coo(:,clip_origin(v,5)), clip_origin(v,5), x0, n0, N0, C0, 2, nN_m);
+    P_A = linear_project_onto_plane(s_coo(:,clip_origin(v,2)), s_nod(clip_origin(v,2)), x0, n0, N0, C0, 1, nN_m);
+    P_B = linear_project_onto_plane(s_coo(:,clip_origin(v,3)), s_nod(clip_origin(v,3)), x0, n0, N0, C0, 1, nN_m);
+    P_C = linear_project_onto_plane(m_coo(:,clip_origin(v,4)), m_nod(clip_origin(v,4)), x0, n0, N0, C0, 2, nN_m);
+    P_D = linear_project_onto_plane(m_coo(:,clip_origin(v,5)), m_nod(clip_origin(v,5)), x0, n0, N0, C0, 2, nN_m);
     Ptilde_A = linear_rotate_points(R, Ralg, proj_s_coo(:,clip_origin(v,2)), P_A, x0, C0);
     Ptilde_B = linear_rotate_points(R, Ralg, proj_s_coo(:,clip_origin(v,3)), P_B, x0, C0);
     Ptilde_C = linear_rotate_points(R, Ralg, proj_m_coo(:,clip_origin(v,4)), P_C, x0, C0);
