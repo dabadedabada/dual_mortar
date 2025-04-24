3cb2e59cb70246e2c17ccea373eb76372378a75a
Jan Kowalczyk <jan.kowalczyk@protonmail.com>
2025-04-24 10:37:27 +0200

    new linearization of disc gap function for single gp

--- linearizations/linear_disc_gapf.m
+++ linearizations/linear_disc_gapf.m
@@ -1,19 +1,25 @@
-function delt_disc_gapf = linear_disc_gapf(s_nod, m_nod, delt_s_nod, delt_m_nod, s_fe, m_fe, delt_avg_normals, avg_normals, s_delt_proj_gp, m_delt_proj_gp, s_proj_gp, m_proj_gp)
+function ghat = linear_disc_gapf(Ngp, normal_gp, G_m, G_s, s_proj_gp, m_proj_gp, s_coo, m_coo, s_nod, m_nod, s_fe, m_fe, nN_m)
 
-%Ns_in_sgp = s_fe.N(s_proj_gp');
-%Nm_in_mgp = m_fe.N(m_proj_gp');
-%disc_gapf = dot(Ns_in_sgp'*s.normals, Nm_in_mgp'*m.nod-Ns_in_sgp'*s.nod, 2); % (A9) Popp 3D 
+C_s = zeros(size(G_s));
+C_m = zeros(size(G_m));
 
-n_gp = size(s_delt_proj_gp,1);
-delt_disc_gapf = zeros(n_gp, 1);
+nN_ele_s = length(s_nod);
+nN_ele_m = length(m_nod);
 
-for i=1:n_gp
-  Ns_in_sgp = s_fe.N(s_proj_gp(i,:)');
-  Nm_in_mgp = m_fe.N(m_proj_gp(i,:)');
-  dNs_in_sgp = s_fe.dNdxi(s_proj_gp(i,:)');
-  dNm_in_mgp = m_fe.dNdxi(m_proj_gp(i,:)');
-  term1 = dot(s_delt_proj_gp(i,:)*dNs_in_sgp'*avg_normals+Ns_in_sgp'*delt_avg_normals, Nm_in_mgp'*m_nod-Ns_in_sgp'*s_nod);
-  term2 = dot(Ns_in_sgp'*avg_normals, m_delt_proj_gp(i,:)*dNm_in_mgp'*m_nod+Nm_in_mgp'*delt_m_nod);
-  term3 = dot(Ns_in_sgp'*avg_normals, s_delt_proj_gp(i,:)*dNs_in_sgp'*s_nod+Ns_in_sgp'*delt_s_nod);
-  delt_disc_gapf(i)=term1+term2-term3; % Popp 3D (A10)
+dNs_in_sgp = s_fe.dNdxi(s_proj_gp);
+dNm_in_mgp = m_fe.dNdxi(m_proj_gp);
+
+Ns_in_sgp = s_fe.N(s_proj_gp);
+Nm_in_mgp = m_fe.N(m_proj_gp);
+
+for k=1:nN_ele_s
+  con_id = nN_m*3 + s_nod(k)*3-2;
+  C_s(:,con_id:con_id+2) = eye(3)*Ns_in_sgp(k);
 end
+for l=1:nN_ele_m
+  con_id = m_nod(l)*3-2;
+  C_m(:,con_id:con_id+2) = eye(3)*Nm_in_mgp(l);
+end
+
+ghat = (m_coo*Nm_in_mgp-s_coo*Ns_in_sgp)'*Ngp + normal_gp'*(m_coo*dNm_in_mgp)*G_m(1:2,:)...
+  -normal_gp'*(s_coo*dNs_in_sgp)*G_s(1:2,:)+normal_gp'*C_m-normal_gp'*C_s;
