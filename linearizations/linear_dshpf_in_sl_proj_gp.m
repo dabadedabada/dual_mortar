9783a0aca5fe8946741ad02a962818cd57c5db4f
Jan Kowalczyk <jan.kowalczyk@protonmail.com>
2025-04-24 10:37:46 +0200

    the size was wrong

--- linearizations/linear_dshpf_in_sl_proj_gp.m
+++ linearizations/linear_dshpf_in_sl_proj_gp.m
@@ -4,6 +4,6 @@ N_in_proj_gp = fe.N(proj_gp);
 dN_in_proj_gp = fe.dNdxi(proj_gp);
 ddshpf_in_proj_gp = Ae*dN_in_proj_gp;
 
-Fs = [zeros(3,3*nN_m),squeeze(pagemtimes(Aalg,N_in_proj_gp))];
+Fs = [zeros(size(N_in_proj_gp,1),3*nN_m),squeeze(pagemtimes(Aalg,N_in_proj_gp))];
 
 F = Fs + ddshpf_in_proj_gp*G(1:2,:);
